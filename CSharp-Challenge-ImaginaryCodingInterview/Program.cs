5e297efcd903739b92041359e73e5ce56d07a4c6
Zachary Hunt <huntzachary30@gmail.com>
2022-09-02 13:24:06 -0500

    Initial commit. -KittehKun

---
+++ CSharp-Challenge-ImaginaryCodingInterview/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Based on the edabit coding challenge found here: https://edabit.com/challenge/dZeNE4BJhyNgA99Fq
+ */
+
+Console.WriteLine("Hello, World!");
