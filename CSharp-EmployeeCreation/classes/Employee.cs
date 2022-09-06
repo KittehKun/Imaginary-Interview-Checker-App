e8af3616c0e18952a0f1d7bcc65d3d503910c317
Zachary Hunt <huntzachary30@gmail.com>
2022-09-06 10:36:44 -0500

    Added new project files.

---
+++ CSharp-EmployeeCreation/classes/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CSharp_Challenge_ImaginaryCodingInterview;
+
+namespace CSharp_EmployeeCreation.classes
+{
+    public class Employee : IEmployee
+    {
+        // - Interface -
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int LastSSN { get; private set; }
+
+
+        // - Constructor -
+        public Employee()
+        {
+
+        }
+
+        
+    }
+}
+
---
+++ CSharp-EmployeeCreation/interfaces/IEmployee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_EmployeeCreation
+{
+    public interface IEmployee
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public int LastSSN { get; }
+    }
+}
+
